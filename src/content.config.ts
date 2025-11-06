fe2adc2539143d88b3f80404f2f002d80c60c8a1
kft12 <undertheunbershine@yahoo.co.jp>
2025-11-06 20:26:36 +0900

    お試し

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,9 +8,9 @@ const blog = defineCollection({
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
-			description: z.string(),
+			description: z.string().optional(),
 			// Transform string to Date object
-			pubDate: z.coerce.date(),
+			date: z.coerce.date().optional(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: image().optional(),
 		}),
